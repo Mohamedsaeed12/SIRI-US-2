2929ef2d0dcd64e779ec4224bbb08ccb5f95dcbb
hfcuser <hfcuser@10-126-52-55.0200.hfc.us.iosda.org>
2026-07-14 19:30:06 -0400

    init commit, new page for team

---
+++ SIRI-US-2/page2.swift
@@ -0,0 +1,18 @@
+//
+//  page2.swift
+//  SIRI-US-2
+//
+//  Created by hfcuser on 7/14/26.
+//
+
+import SwiftUI
+
+struct page2: View {
+    var body: some View {
+        Text("Mohammed Sabr")
+    }
+}
+
+#Preview {
+    page2()
+}
